6291b6e727441ca6edc45a21f29356b22b734eaa
Justinas Jankevicius <justin.jank@gmail.com>
2019-03-30 12:02:07 +0200

    better function names, return to menu after game

--- hangman.py
+++ hangman.py
@@ -4,7 +4,7 @@ import random
 
 def main():
     # w - word to guess.
-    def get_w():
+    def get_input_word():
         while True:
             w = getpass.getpass("Enter a word to guess: ").lower()
             if w.isalpha():
@@ -20,7 +20,7 @@ def main():
                 print("Please, use only letters.")
 
     # g - your guess, a letter.
-    def get_g():
+    def get_guess():
         while True:
             g = input("Guess a letter: ").lower()
             if g.isalpha():
@@ -38,7 +38,7 @@ def main():
                 print("Please, use only one letter.")
 
     # d - difficulty, the number of wrong guesses.
-    def get_d():
+    def get_difficulty():
         while True:
             d = input("Select a difficulty. Enter a number of wrong guesses: ")
             if d == "":
@@ -61,55 +61,50 @@ def main():
             else:
                 print("Please, enter a whole number greater than 0.")
 
-    def eng():
-        lst = []
-        for i in range(97, 123):
-            lst.append(chr(i))
-        return lst
-
-    def wtl(w):
+    def word_to_list(w):  # "wheel" ---> "[w, h, e, e, l]"
         p = []
         for i in range(0, len(w)):
             p.append(w[i])
         return p
 
-    def wtp(w):
+    def word_to_puzzle(w):  # "wheel" ---> "[" _ "," _ "," _ "," _ "," _ "]"
         p = []
         for i in range(0, len(w)):
             p.append(" _ ")
         return p
 
-    def pts(p):
+    def puzzle_to_string(p):  # "[w, h, e, e, l]" ---> " _  _  _  _  _ "
         s = ""
         for i in range(0, len(p)):
             s += p[i]
         return s
 
     def menu():
-        print('\n*********HANGMAN GAME D16**********\n')
-        print('MENU:\n1. Guess input word\n2. Guess random word')
-        print('3. Guess a riddle\n4. Quit game\n')
-        play_mode = 0
-        while play_mode not in ["1", "2", "3", "4"]:
-            play_mode = input('Please select your option (1, 2, 3 or 4): ')
-            if play_mode not in ["1", "2", "3", "4"]:
-                print("Incorrect! Try again.")
-        if play_mode == "1":
-            guess_word()
-        elif play_mode == "2":
-            guess_random_word()
-        elif play_mode == "3":
-            guess_riddle()
-        elif play_mode == "4":
-            print("Goodbye!")
-            return
+        while True:
+            print('\n*********HANGMAN GAME D16**********\n')
+            print('MENU:\n1. Guess input word\n2. Guess random word')
+            print('3. Guess a riddle\n4. Quit game\n')
+            play_mode = 0
+            while play_mode not in ["1", "2", "3", "4"]:
+                play_mode = input('Please select your option (1, 2, 3 or 4): ')
+                if play_mode not in ["1", "2", "3", "4"]:
+                    print("Incorrect! Try again.")
+            if play_mode == "1":
+                guess_word()
+            elif play_mode == "2":
+                guess_random_word()
+            elif play_mode == "3":
+                guess_riddle()
+            elif play_mode == "4":
+                print("Goodbye!")
+                return
 
     def guess_word():
-        w = get_w()
+        w = get_input_word()
         if w == 1:
             print("See you next time.")
             return
-        d = get_d()
+        d = get_difficulty()
         if d == 1:
             print("See you next time.")
             return
@@ -117,14 +112,14 @@ def main():
         return
 
     def guess_riddle():
-        d = get_d()
+        d = get_difficulty()
         if d == 1:
             print("See you next time.")
             return
         ctg = ""
-        while ctg not in ["daiktai", "gamta", "gyvūnai"]:
-            ctg = input('Select category (daiktai, gamta, gyvūnai): ').lower()
-            if ctg not in ["daiktai", "gamta", "gyvūnai"]:
+        while ctg not in ["items", "nature", "animals"]:
+            ctg = input('Select category (items, nature, animals): ').lower()
+            if ctg not in ["items", "nature", "animals"]:
                 print("Incorrect! Try again.")
         dct = {}
         f = open("riddles.txt", "r")
@@ -144,7 +139,7 @@ def main():
         return
 
     def guess_random_word():
-        d = get_d()
+        d = get_difficulty()
         if d == 1:
             print("See you next time.")
             return
@@ -154,14 +149,14 @@ def main():
         return
 
     def check_guesses(answer, d):
-        l_ans = wtl(answer)
-        p = wtp(answer)
-        s = pts(p)
+        l_ans = word_to_list(answer)  # list containing answer split to chars
+        p = word_to_puzzle(answer)
+        s = puzzle_to_string(p)
         c = 0
         print("\n"+s+"\n")
         while c < d:
             draw_gallows(d-c)
-            g = get_g()
+            g = get_guess()
             if g == 1:
                 print("See you next time.")
                 break
@@ -172,7 +167,7 @@ def main():
                     f = answer.find(g, f + 1)
                 print("\n\n"+g.upper(), "is in a word!\t", end="")
                 print("Number of wrong answers left:", d-c)
-                s = pts(p)
+                s = puzzle_to_string(p)
                 print("\n"+s+"\n")
                 if "_" in s:
                     pass
@@ -185,9 +180,11 @@ def main():
                 print("Number of wrong guesses left:", d-c)
                 if d-c == 0:
                     draw_gallows(d-c)
-                    print("Game over. Correct answer was:", pts(l_ans).upper())
+                    print("Game over. Correct answer was:", end="")
+                    print(puzzle_to_string(l_ans).upper())
                 else:
-                    print("\n" + pts(p) + "\n")
+                    print("\n" + puzzle_to_string(p) + "\n")
+        input("(Press any key to continue.)")
         return
 
     def draw_gallows(d):
